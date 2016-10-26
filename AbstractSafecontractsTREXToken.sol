6406d279e72bcd6048e91a9a236f1ad60fcb2c6c
SafeContracts-DAO <webmaster@safecontracts.com>
2016-10-26 15:06:50 -0500

    Update AbstractSafecontractsTREXToken.sol

--- AbstractSafecontractsTREXToken.sol
+++ AbstractSafecontractsTREXToken.sol
@@ -2,6 +2,7 @@ pragma solidity ^0.4.0;
 
 // Title AbstractSafecontractsTREXToken.sol
 // Customize @author Rocky Fikki - <rocky@fikki.net>
+// Credit - https://github.com/ConsenSys/singulardtv-contracts
 
 import "AbstractToken.sol";
 
