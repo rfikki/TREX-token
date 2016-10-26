0c7a80d18a07126b62886b8f0f73351b289b6abc
SafeContracts-DAO <webmaster@safecontracts.com>
2016-10-26 15:06:17 -0500

    Update AbstractSafecontractsTREXFund.sol

--- AbstractSafecontractsTREXFund.sol
+++ AbstractSafecontractsTREXFund.sol
@@ -2,6 +2,7 @@ pragma solidity ^0.4.0;
 
 // Title AbstractSafecontractsTREXFund.sol
 // Customize @author Rocky Fikki - <rocky@fikki.net>
+// Credit - https://github.com/ConsenSys/singulardtv-contracts
 
 contract AbstractSafecontractsTREXFund {
     function trexdevshop() returns (address);
