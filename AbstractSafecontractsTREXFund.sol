076f8f723fd18413b118caca08d5ce735976dfc7
SafeContracts-DAO <webmaster@safecontracts.com>
2016-10-26 14:52:17 -0500

    Create AbstractSafecontractsTREXFund.sol

---
+++ AbstractSafecontractsTREXFund.sol
@@ -0,0 +1,9 @@
+pragma solidity ^0.4.0;
+
+// Title AbstractSafecontractsTREXFund.sol
+// Customize @author Rocky Fikki - <rocky@fikki.net>
+
+contract AbstractSafecontractsTREXFund {
+    function trexdevshop() returns (address);
+    function softWithdrawRevenueFor(address forAddress) returns (uint);
+}
