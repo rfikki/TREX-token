ad54010e32fefead74736314efdc65b0ad00a900
SafeContracts-DAO <webmaster@safecontracts.com>
2016-10-26 14:51:22 -0500

    Create AbstractSafecontractsTREXCrowdfunding.sol

---
+++ AbstractSafecontractsTREXCrowdfunding.sol
@@ -0,0 +1,14 @@
+pragma solidity ^0.4.0;
+
+// Title AbstractSafecontractsTREXCrowdfunding.sol
+// Customize @author Rocky Fikki - <rocky@fikki.net>
+
+contract AbstractSafecontractsTREXCrowdfunding {
+    function trexdevshopWaited1Years() returns (bool);
+    function startDate() returns (uint);
+    function CROWDFUNDING_PERIOD() returns (uint);
+    function TOKEN_TARGET() returns (uint);
+    function valuePerShare() returns (uint);
+    function fundBalance() returns (uint);
+    function campaignEndedSuccessfully() returns (bool);
+}
